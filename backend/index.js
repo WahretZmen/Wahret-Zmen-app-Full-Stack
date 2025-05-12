d7686823646c060f25436c1564b600fcff689c81
WahretZmen <wahretzmensabri521@gmail.com>
2025-05-12 13:01:59 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 5000;
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
   : [
-      "http://localhost:5173",
+      "http://localhost:5173", 
+    "https://wahret-zmen-app-frontend-flame.vercel.app",
       
       
       
